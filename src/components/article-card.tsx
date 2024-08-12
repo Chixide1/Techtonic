2111b5f17ba4e2f0046ad5c4a5e50a439359dcba
Chik <chixide12@gmail.com>
2024-08-12 21:49:19 +0100

    style: - changed max width on article card title

--- src/components/article-card.tsx
+++ src/components/article-card.tsx
@@ -15,13 +15,13 @@ export function ArticleCard(article: ArticlesResponse) {
         <Image className="w-full md:max-w-56 rounded-lg" src={imgSrc} alt="Article image" width={200} height={1} />
         <div className="flex flex-col gap-4 flex-grow justify-between w-auto">
           <div className="flex w-full gap-6">
-            <div className="text-sm font-semibold flex w-full max-w-[15ch]">
+            <div className="text-sm font-semibold flex w-full max-w-[25ch]">
               <LayersIcon className="bg-violet-300 mr-1 max-w-5 text-violet-800 p-0.5 rounded-sm dark:bg-violet-800 dark:text-violet-100"/>
               <span className="overflow-hidden whitespace-nowrap text-ellipsis">{article.category}</span>
             </div>
-            <span className="ml-auto text-gray-600 text-sm max-w-[15ch] whitespace-nowrap overflow-hidden text-ellipsis dark:text-gray-400">{formattedArticleCreated}</span>
+            <span className="ml-auto text-gray-600 text-sm max-w-[25ch] whitespace-nowrap overflow-hidden text-ellipsis dark:text-gray-400">{formattedArticleCreated}</span>
           </div>
-          <h5 className="text-lg font-semibold md:max-w-xl md:text-balance md:w-8/12">{article.title}</h5>
+          <h5 className="text-lg font-semibold md:max-w-xl md:text-balance">{article.title}</h5>
           <div className="flex w-full justify-between">
             <span className="text-sm">Read More &rarr;</span>
             <span className="flex gap-2 text-gray-600 text-sm dark:text-gray-400"><EyeOpenIcon strokeWidth={5} className="max-w-4" />{article.views.toLocaleString()}</span>
