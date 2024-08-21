92a1b2ff77e63481299f514c1efee8597a74b78d
Chik <chixide12@gmail.com>
2024-08-21 16:57:58 +0100

    styele: - removed border from sections

--- src/components/article-body.tsx
+++ src/components/article-body.tsx
@@ -12,13 +12,13 @@ type ArticleBodyProps = {
 export function ArticleBody({sections}: ArticleBodyProps ){
   const [activeId, setActiveId] = useState('')
   let refElements = useObserver(setActiveId)
-  
+
   return (
     <div className="flex flex-col-reverse gap-8 lg:flex-row mt-28">
       <article className="lg:max-w-[75%]">
         {sections.map((section, i) => (
           <section key={section.id} id={section.id} ref={(el) => {refElements.current[i] = el}}
-          className="px-6 py-8 border border-border rounded-lg my-4 bg-card scroll-mt-14 shadow-sm">
+          className="px-6 py-8 dark:border border-border rounded-lg my-4 bg-card scroll-mt-14">
             <h1 className='text-xl font-semibold mb-2'>{section.heading}</h1>
             <div className="text-muted-foreground" dangerouslySetInnerHTML={{ __html: section.content }} />
           </section>
