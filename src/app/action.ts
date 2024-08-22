793d65652e984e13e0f7589c37ede943ffe1cbd1
Chik <chixide12@gmail.com>
2024-08-22 20:33:55 +0100

    refactor: -added env var for pocketbase

--- src/app/action.ts
+++ src/app/action.ts
@@ -1,27 +1,28 @@
 'use server'
 import PocketBase from 'pocketbase';
 import { ArticlesRecord, RecordIdString, SectionsRecord, TypedPocketBase } from "@/lib/pocketbase-types";
+const pb_url = process.env.PB_URL
 
 export async function getArticles(page: number = 1, perPage: number = 5){
-  const pb = new PocketBase('http://127.0.0.1:8090') as TypedPocketBase;
+  const pb = new PocketBase(pb_url) as TypedPocketBase;
   const articles = await pb.collection('articles').getList(page, perPage);
   return articles
 }
 
 export async function getArticle(id: RecordIdString){
-  const pb = new PocketBase('http://127.0.0.1:8090') as TypedPocketBase;
+  const pb = new PocketBase(pb_url) as TypedPocketBase;
   const article = await pb.collection('articles').getOne(id, { expand: 'sections'})
   return article as ArticlesRecord<{sections: SectionsRecord[]}>
 }
 
 export async function getImgSrc(record: ArticlesRecord<any>, imgName: string) {
-  const pb = new PocketBase('http://127.0.0.1:8090') as TypedPocketBase;
+  const pb = new PocketBase(pb_url) as TypedPocketBase;
   let url = pb.files.getUrl(record, imgName)
   return url
 }
 
 export async function filterArticles(query: string){
-  const pb = new PocketBase('http://127.0.0.1:8090') as TypedPocketBase;
+  const pb = new PocketBase(pb_url) as TypedPocketBase;
   const results = pb.collection('articles').getList(1, 10, {
     filter: `title ~ "${query}"`
   })
@@ -29,7 +30,7 @@ export async function filterArticles(query: string){
 }
 
 export async function incrView(id: RecordIdString){
-  const pb = new PocketBase('http://127.0.0.1:8090') as TypedPocketBase;
+  const pb = new PocketBase(pb_url) as TypedPocketBase;
  
   const updatedRecord = await pb.send(`/addView/${id}`, {method: 'GET'})
   return updatedRecord
