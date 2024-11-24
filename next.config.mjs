bf7fea5e3c027ce39c194efc526d48f3594d4d5d
Chik <chixide12@gmail.com>
2024-11-24 22:17:56 +0000

    _

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,16 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: '**.ckdoestech.com',
+            port: '',
+            pathname: '/**',
+          },
+        ]
+      },
 }
 
 export default withPayload(nextConfig)
