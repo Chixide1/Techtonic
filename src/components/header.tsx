bb96ee0f7673afa2a5abac9939f5eea49706e8a5
Chik <chixide12@gmail.com>
2024-11-26 20:09:47 +0000

    Changed the blog name

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,7 +9,7 @@ export function Header() {
     <header className='flex gap-12 justify-between items-center w-full dark:bg-background/80 backdrop-blur-sm p-3 px-8 max-w-full sticky top-0 z-30'>
       <Link href={'/'} className='flex items-center'>
         <CrumpledPaperIcon className='mr-2 w-6'/>
-        <h5 className='font-semibold tracking-tight hidden sm:inline'>Example Blog App</h5>
+        <h5 className='font-semibold tracking-tight hidden sm:inline'>Techtonic</h5>
         <span className='text-neutral-600 dark:text-neutral-400 pl-2 whitespace-nowrap font-semibold hidden md:inline'>/ Blog</span>
       </Link>
       <div className='flex flex-shrink-0 gap-4'>
