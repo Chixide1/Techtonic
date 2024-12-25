4816e4371f72ef44b2b299aafa44706b75415536
Chikezie <chixide12@gmail.com>
2024-12-25 02:52:44 +0000

    style: - added title and switched header logo

--- src/app/(frontend)/article/[id]/page.tsx
+++ src/app/(frontend)/article/[id]/page.tsx
@@ -2,6 +2,8 @@ import { ArticleBody } from '@/components/article-body'
 import { ArticleHeader } from '@/components/article-header'
 import { addView, getArticle } from '@/app/action'
 import { notFound } from 'next/navigation'
+import Head from 'next/head'
+import { Metadata } from 'next'
 
 type ArticleParams = {
   params: Promise<{id: number}>
@@ -15,8 +17,21 @@ export default async function Page(data: ArticleParams){
 
   return (
     <main className='px-6 md:px-10'>
+      <Head>
+        <title>{article.title}</title>
+      </Head>
       <ArticleHeader article={article}/>
       <ArticleBody article={article}/>
     </main>
   )
+}
+
+export async function generateMetadata(data: ArticleParams): Promise<Metadata> {
+  const id = (await data.params).id
+  const article = await getArticle(id).catch(() => notFound())
+
+  return {
+    title: article.title,
+    description: article.category,
+  };
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,14 @@
-import { CrumpledPaperIcon, UpdateIcon } from '@radix-ui/react-icons'
 import { GithubButton } from './github-button'
 import { ThemeButton } from './theme-button'
 import Link from 'next/link'
 import { SearchButton } from './search-button'
+import logo from "public/favicon.ico"
 
 export function Header() {
   return (
     <header className='flex gap-12 justify-between items-center w-full dark:bg-background/80 backdrop-blur-sm p-3 px-8 max-w-full sticky top-0 z-30'>
       <Link href={'/'} className='flex items-center'>
-        <CrumpledPaperIcon className='mr-2 w-6'/>
+        <img src={logo.src} alt='The logo of the website' className='mr-2 w-5'/>
         <h5 className='font-semibold tracking-tight hidden sm:inline'>Techtonic</h5>
         <span className='text-neutral-600 dark:text-neutral-400 pl-2 whitespace-nowrap font-semibold hidden md:inline'>/ Blog</span>
       </Link>
